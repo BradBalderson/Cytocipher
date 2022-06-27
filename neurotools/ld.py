6d1983eca31071350b22353cd88bbc8184d1b6eb
bradbal <s4392586@student.uq.edu.au>
2022-06-27 16:30:54 +1000

    Added ability to load Synapse related CAMs

--- neurotools/ld.py
+++ neurotools/ld.py
@@ -6,3 +6,4 @@ from .activation.act_quantify import load_iegs
 from .preprocessing.balanced_feature_selection import load_nps
 from .preprocessing.balanced_feature_selection import load_sex
 from .preprocessing.balanced_feature_selection import load_tfs
+from .preprocessing.balanced_feature_selection import load_cams
