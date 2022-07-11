2fb05c0ad634a3209be3949a466c410524b94606
bradbal <s4392586@student.uq.edu.au>
2022-07-11 16:54:14 +1000

    Added plots for comparing between annotations.

--- neurotools/plotting/cluster_diagnostics.py
+++ neurotools/plotting/cluster_diagnostics.py
@@ -13,6 +13,10 @@ from sklearn.preprocessing import minmax_scale
 import matplotlib.pyplot as plt
 import seaborn as sb
 
+################################################################################
+                    # Coexpression scoring plots #
+################################################################################
+
 def enrich_heatmap(data: AnnData, groupby: str, per_cell: bool=True,
                    plot_group: str=None, figsize=(12, 12),
                    dendrogram: bool=False, vmax=1, show=True,
@@ -78,7 +82,90 @@ def plot_cluster_sils(data: AnnData, groupby: str):
     ax.axhline(y=np.mean(scores))
     plt.show()
 
+################################################################################
+                    # Comparing cluster plots #
+################################################################################
+def annot_overlap_barplot(data: sc.AnnData, groupby1: str, groupby2: str,
+                          uns_key: str = 'annot_overlaps',
+                          bar_width: float = .85,
+                          figsize=(10, 5), scale: bool = True,
+                          transpose: bool = False,
+                          remove_labelling: bool = False):
+    """Plots relationship between annotations by showing proportions of each type found within
+        each of the other categories.
+    """
+    if uns_key not in data.uns:
+        print("Need to run get_overlap_counts() first.")
 
+    overlap_counts_df = data.uns[uns_key]
+    if transpose:  ##### Looking at relationship in opposite direction.
+        overlap_counts_df = overlap_counts_df.transpose()
+        tmp = groupby1
+        groupby1 = groupby2
+        groupby2 = tmp
 
+    labelset1 = overlap_counts_df.index.values
+    labelset2 = overlap_counts_df.columns.values
+
+    ##### Getting colors
+    if f'{groupby2}_colors' in data.uns:
+        colors = {val: data.uns[f'{groupby2}_colors'][i]
+                  for i, val in enumerate(data.obs[groupby2].cat.categories)}
+    else:
+        colors = {val: 'grey' for
+                  i, val in enumerate(data.obs[groupby2].cat.categories)}
+
+    ##### Scaling the data to proportions.
+    if scale:
+        total_counts = overlap_counts_df.sum(axis=1)
+        overlap_counts = np.apply_along_axis(np.divide, 0,
+                                             overlap_counts_df.values,
+                                             total_counts)
+    else:
+        overlap_counts = overlap_counts_df.values
+
+    ##### Creating the barplots...
+    fig, axs = plt.subplots(figsize=figsize)
+    col_locs = list(range(len(labelset1)))
+    for labeli, label in enumerate(labelset2):
+        props = overlap_counts[:, labeli]
+
+        if labeli == 0:
+            bottom = [0] * len(props)
+
+        axs.bar(col_locs, props, bottom=bottom,
+                color=colors[label], edgecolor='white',
+                width=bar_width, )
+        bottom = list(np.array(bottom) + props)
+
+        # Custom x axis
+        axs.set_xticks(list(range(len(labelset1))), list(labelset1),
+                       rotation=90)
+        axs.spines['right'].set_visible(False)
+        axs.spines['top'].set_visible(False)
+        if remove_labelling:
+            axs.set_xticks([])
+            axs.set_yticks([])
+            axs.spines['left'].set_visible(False)
+            axs.spines['bottom'].set_visible(False)
+        else:
+            axs.set_xlabel(f"{groupby1} Clusters")
+
+def annot_overlap_heatmap(data: sc.AnnData, overlap_key: str,
+                          show: bool=True):
+    """Plots a heatmap of the number of cells overlapping two sets of anotations.
+    """
+    overlap_counts = minmax_scale(data.uns[overlap_key].values, axis=0)
+    overlap_counts = pd.DataFrame(overlap_counts,
+                                  index=data.uns[overlap_key].index,
+                                  columns=data.uns[overlap_key].columns)
+
+    cluster_overlaps = sc.AnnData(overlap_counts)
+    cluster_overlaps.obs['sc_clusters'] = cluster_overlaps.obs_names.values
+    cluster_overlaps.obs['sc_clusters'] = cluster_overlaps.obs[
+                                               'sc_clusters'].astype('category')
+    sc.pl.matrixplot(cluster_overlaps,
+                     var_names=cluster_overlaps.var_names.values,
+                     groupby='sc_clusters', show=show)
 
 
