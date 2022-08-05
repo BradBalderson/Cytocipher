76aa91659c5cb94e5b00b14cd7e39c9a25430e1d
bradbal <s4392586@student.uq.edu.au>
2022-08-05 19:48:55 +1000

    Added key functions to interface

--- neurotools/pl.py
+++ neurotools/pl.py
@@ -1,4 +1,7 @@
 
-from .plotting.act_plots import ieg_plot, ieg_counts
+#from .plotting.act_plots import ieg_plot, ieg_counts
+
+from .plotting.cluster_diagnostics import enrich_heatmap, \
+                                                         sig_cluster_diagnostics
 
 
--- neurotools/tl.py
+++ neurotools/tl.py
@@ -1,4 +1,8 @@
+#from .activation.act_quantify import ieg_activation
 
-from .activation.act_quantify import ieg_activation
+#### For cluster scoring ####
+from .cluster.cluster_score import code_enrich
 
+#### For cluster merginging ####
+from .cluster.cluster_label import merge_clusters, get_markers
 
