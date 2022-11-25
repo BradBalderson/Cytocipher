989ed5c3b19d422b427ab46a3de4bc8f906e39ad
bradbal <s4392586@student.uq.edu.au>
2022-11-25 16:51:12 +1000

    Added capability of determining cluster MNNs in a highly general way, by making use of sc.pp.neighbors and the use_rep param

--- cytocipher/score_and_merge/_neighbors.py
+++ cytocipher/score_and_merge/_neighbors.py
@@ -31,7 +31,8 @@ def general_neighbours(data: sc.AnnData,
            pd.DataFrame(clust_dists, index=label_set, columns=label_set)
 
 #@jit(parallel=True, forceobj=True, nopython=False)
-@njit #(parallel=True)
+#@njit #(parallel=True)
+@jit(parallel=True, forceobj=True, nopython=False)
 def get_neighs_FAST(labels: np.array, label_set: np.array,
                     knn_adj_matrix: np.ndarray,
                     mnn_frac_cutoff: float):
