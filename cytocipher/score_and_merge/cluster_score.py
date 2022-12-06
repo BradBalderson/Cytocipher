3be510a32c2d2fc170c5e6d3d637de0c1ac08ba0
bradbal <s4392586@student.uq.edu.au>
2022-12-06 16:52:07 +1000

    Found some edge case...

--- cytocipher/score_and_merge/cluster_score.py
+++ cytocipher/score_and_merge/cluster_score.py
@@ -499,8 +499,8 @@ def get_code_scores(full_expr: np.ndarray, all_genes: np.array,
                 for clustj in range(len(clusts_diff)):
                     ### Added in accounting for the end of the list...
                     ###### Turns out this only happens if
-                    #if (clusts_diff[clustj]==clust and (clustj+1)==len(clusts_diff)) or \
-                     if (clusts_diff[clustj]==clust and clusts_diff[clustj+1]!=clust):
+                    if (clusts_diff[clustj]==clust and (clustj+1)==len(clusts_diff)) or \
+                       (clusts_diff[clustj]==clust and clusts_diff[clustj+1]!=clust):
                         negative_indices[clusti] = clustj
                         break
 
