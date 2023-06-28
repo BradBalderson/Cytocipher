794150a6bc36b892d380027ccda5e3966fef36d0
Brad Balderson <brad.balderson@uqconnect.edu.au>
2023-06-28 19:10:46 +1000

    Updated Cytocipher version to 0.1.20

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 python setup.py sdist
 #python setup.py sdist --manifest-only
 python setup.py bdist_wheel --universal
-#twine upload --repository-url https://test.pypi.org/legacy/ dist/cytocipher-0.1.15.tar.gz
-twine upload dist/cytocipher-0.1.15.tar.gz
+#twine upload --repository-url https://test.pypi.org/legacy/ dist/cytocipher-0.1.20.tar.gz
+twine upload dist/cytocipher-0.1.20.tar.gz
 
 ### Downloading for test
 ## Instructions from: https://stackoverflow.com/questions/34514703/pip-install-from-pypi-works-but-from-testpypi-fails-cannot-find-requirements
--- cytocipher/__init__.py
+++ cytocipher/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Brad Balderson"""
 __email__ = "brad.balderson@uqconnect.edu.au"
-__version__ = "0.1.15"
+__version__ = "0.1.20"
 
 from . import tl
 from . import pl
