963a814be6a19345e667716449710593b15ce22a
Brad Balderson <brad.balderson@uqconnect.edu.au>
2023-08-10 14:26:56 +1000

    Final changes for documentation.

--- build.sh
+++ build.sh
@@ -9,5 +9,9 @@ twine upload dist/cytocipher-0.1.20.tar.gz
 ## Instructions from: https://stackoverflow.com/questions/34514703/pip-install-from-pypi-works-but-from-testpypi-fails-cannot-find-requirements
 #pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple cytocipher==0.1.2
 
+###### For updating the documentation:
+#Edit documentation in: mkdocs.yml and cytocipher/docs/
 
+mkdocs build
+mkdocs gh-deploy --clean # deploys to github.
 
